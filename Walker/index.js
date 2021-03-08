750d0e9c911724e87dda8189536f3c0e657dbe78
JocelynneCrandall <crandall50849@students.apsb.org>
2021-03-08 18:54:42 +0000

    saving pong

--- Walker/index.js
+++ Walker/index.js
@@ -12,11 +12,22 @@ function runProgram(){
   var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
   
   // Game Item Objects
-
+var KEY = {
+  "LEFT": 37,
+}
+var KEY = {
+  "RIGHT": 39,
+}
+var KEY = {
+  "UP": 38,
+}
+var KEY = {
+  "DOWN": 40,
+}
 
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
+  $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -34,8 +45,8 @@ function runProgram(){
   /* 
   Called in response to events.
   */
-  function handleEvent(event) {
-
+  function handleKeyDown(event) {
+      console.log(event.which);
   }
 
   ////////////////////////////////////////////////////////////////////////////////
