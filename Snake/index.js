6f2ce9685da182024efdb0f635488cd0562c0761
JocelynneCrandall <crandall50849@students.apsb.org>
2021-04-14 22:35:47 +0000

    Pong

--- Snake/index.js
+++ Snake/index.js
@@ -10,28 +10,8 @@ function runProgram(){
   // Constant Variables
   var FRAME_RATE = 60;
   var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
-
+  
   // Game Item Objects
-var player1 = {};
-player1.$element = $('#player1');
-player1.x = 10;
-player1.y = 225;
-var speedY1 = {};
-
-var player2 = {};
-player2.$element = $("#player2");
-player2.x = 1170;
-player2.y = 225;
-var speedY2 = {};
-
-var ball = {};
-ball.$element = $("#ball");
-ball.x = 597.5;
-ball.y = 255;
-
-var midpoint = {};
-midpoint.$element = $("#midpoint");
-midpoint.x = 600;
 
 
   // one-time setup
@@ -50,28 +30,7 @@ midpoint.x = 600;
     
 
   }
-  // what each key equals ; taking care of magic numbers
-  var Key = {
-      "Up1": 87, //left player up
-      "Down1": 83, //left player down
-      "Up2": 38, //right player up
-      "Down2": 40, //right player down
-  };
-  function handleKeyDown(event) {
-      if (event.which === Key.Down1) {
-    speedY1 = -5;
-  }
-  else if (event.which === Key.Up1) {
-      speedY1 = 5;
-  }
-  else if (event.which === Key.Down2) {
-      speedY2 = -5;
-  }
-  else if (event.which === Key.Up2) {
-      speedY2 = 5;
-  }
-}
-  }
+  
   /* 
   Called in response to events.
   */
@@ -92,4 +51,4 @@ midpoint.x = 600;
     $(document).off();
   }
   
-
+}
