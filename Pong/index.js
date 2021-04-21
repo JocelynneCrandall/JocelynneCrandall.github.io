55152ffa8c8a29122fc12d346752f051b37c2197
JocelynneCrandall <crandall50849@students.apsb.org>
2021-04-21 23:33:00 +0000

    Pong

--- Pong/index.js
+++ Pong/index.js
@@ -16,13 +16,16 @@ var player1 = {};
 player1.$element = $('#player1');
 player1.x = 10;
 player1.y = 225;
-var speedY1 = {};
+var speedY1 = 0;
+var positionY1 = 225;
 
 var player2 = {};
 player2.$element = $("#player2");
 player2.x = 1170;
 player2.y = 225;
-var speedY2 = {};
+var speedY2 = 0;
+var positionY2 = 225;
+
 
 var ball = {};
 ball.$element = $("#ball");
@@ -37,6 +40,8 @@ midpoint.x = 600;
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
   $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
+  $(document).on("keydown", handleKeyDown);
+  $(document).on("keyup", handleKeyUp);
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -47,42 +52,65 @@ midpoint.x = 600;
   by calling this function and executing the code inside.
   */
   function newFrame() {
-    
-
+    repositionGameItems();
+    redrawGameItems();
   }
   // what each key equals ; taking care of magic numbers
-  var Key = {
+  var KEY = {
       "Up1": 87, //left player up
       "Down1": 83, //left player down
       "Up2": 38, //right player up
       "Down2": 40, //right player down
   };
-  function handleKeyDown(event) {
-      if (event.which === Key.Down1) {
-    speedY1 = -5;
-  }
-  else if (event.which === Key.Up1) {
-      speedY1 = 5;
-  }
-  else if (event.which === Key.Down2) {
-      speedY2 = -5;
-  }
-  else if (event.which === Key.Up2) {
-      speedY2 = 5;
-  }
-}
-  }
+  function handleKeyDown(event) { /// making players move when key pressed
+ if (event.which === KEY.Up1) {
+     speedY1 = -5;
+ }
+ else if (event.which === KEY.Up2) {
+     speedY2 = -5;
+ }
+ else if (event.which === KEY.Down1) {
+     speedY1 = 5;
+ }
+ else if (event.which === KEY.Down2) {
+     speedY2 = 5;
+ }
+};   
+function handleKeyUp(event) {
+    if (event.which === KEY.Up1) {
+     speedY1 = 0;
+ }
+ else if (event.which === KEY.Up2) {
+     speedY2 = 0;
+ }
+ else if (event.which === KEY.Down1) {
+     speedY1 = 0;
+ }
+ else if (event.which === KEY.Down2) {
+     speedY2 = 0;
+ }
+}                        
   /* 
   Called in response to events.
   */
   function handleEvent(event) {
 
   }
-
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
+function repositionGameItems() { // update player positions
+  positionY1 += speedY1
+  positionY2 += speedY2
+}
+function redrawGameItems() { //show player's new positions
+$("#player1").css("top", positionY1);
+$("#player2").css("top", positionY2);
+ }
 
+ function doCollide(player1, player2, ball, board) {  /// stopping/changing movement of items when colliding with eachother/edges
+   
+ }
   
   function endGame() {
     // stop the interval timer
@@ -92,4 +120,4 @@ midpoint.x = 600;
     $(document).off();
   }
   
-
+}
