8c3258f6c69eb09b2959ca6678c5a4994025957b
JocelynneCrandall <crandall50849@students.apsb.org>
2021-05-12 23:23:17 +0000

    image-filtering

--- image-filtering/index.js
+++ image-filtering/index.js
@@ -2,12 +2,9 @@
 // as soon as the page loads and is where you should call your functions.
 $(document).ready(function(){
     const $display = $('#display');
-applyFilter();
     // TODO: Call your apply function(s) here
 
-
-
-
+applyFilter(reddify);
 
     render($display, image);
 });
@@ -17,19 +14,18 @@ applyFilter();
 /////////////////////////////////////////////////////////
 
 // TODO: Create the applyFilter function here
-function applyFilter() {
+ function applyFilter(filterFunction) {  // applies the filters, making them more red, blue, or green
    for(var w = 0; w < image.length; w++) {
      for(var k = 0; k < image[w].length; k++) { 
 
 var rbgString = image[w][k];  
-var rbgNumbers = {};
-rbgNumbers = rgbStringToArray(rbgString);
-rbgNumbers[0] = 255; 
-rbgString = rgbArrayToString(rbgString); 
-image[w][k] = image[w][k] - rbgString; 
+var rbgNumbers = rgbStringToArray(rbgString); 
+filterFunction(rbgNumbers);
+rbgString = rgbArrayToString(rbgNumbers); 
+image[w][k] = rbgString; 
  }
  }
- }
+ } 
 
 // TODO: Create the applyFilterNoBackground function
 function applyFilterNoBackground() {
@@ -37,6 +33,7 @@ function applyFilterNoBackground() {
 }
 
 // TODO: Create filter functions
-
-
+ function reddify(rbgNumbers) {  /// increases value of red in all pictures, making picture only warm colors
+     rbgNumbers[RED] = 255;
+ }
 // CHALLENGE code goes below here
